f67df3a2ad51cafe012e22f3f937ef8f98b96e8b
Budry <zarubaondra@gmail.com>
2015-02-28 11:48:29 +0100

    Added invalid argument exception

---
+++ src/exceptions.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is part of the Kappa\AssetsPublisher package.
+ *
+ * (c) Ondřej Záruba <zarubaondra@gmail.com>
+ *
+ * For the full copyright and license information, please view the license.md
+ * file that was distributed with this source code.
+ */
+
+namespace Kappa\AssetsPublisher;
+
+/**
+ * Class DirectoryNotFoundException
+ *
+ * @package Kappa\AssetsPublisher
+ * @author Ondřej Záruba <http://zaruba-ondrej.cz>
+ */
+class DirectoryNotFoundException extends \LogicException
+{
+
+}
